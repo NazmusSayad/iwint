0e6a9069c1819b798371b82de6d5600ebe6b5eb8
Nazmus Sayad <87106526+NazmusSayad@users.noreply.github.com>
2024-11-09 05:51:56 +0600

    M package.json  M src/addToContextMenu.ts

--- src/addToContextMenu.ts
+++ src/addToContextMenu.ts
@@ -1,6 +1,12 @@
 import { execSync } from 'child_process'
 
-function writeItem(name: string, label: string, command: string, icon: string) {
+function writeItem(
+  name: string,
+  label: string,
+  command: string,
+  icon: string,
+  extended = false
+) {
   execSync(
     `reg add "HKCU\\SOFTWARE\\Classes\\Directory\\shell\\${name}" /ve /t REG_SZ /d "${label}" /f`
   )
@@ -10,6 +16,11 @@ function writeItem(name: string, label: string, command: string, icon: string) {
   execSync(
     `reg add "HKCU\\SOFTWARE\\Classes\\Directory\\shell\\${name}\\command" /ve /t REG_SZ /d "${command}" /f`
   )
+  if (extended) {
+    execSync(
+      `reg add "HKCU\\SOFTWARE\\Classes\\Directory\\shell\\${name}" /v Extended /t REG_SZ /d "" /f`
+    )
+  }
 
   execSync(
     `reg add "HKCU\\SOFTWARE\\Classes\\Directory\\background\\shell\\${name}" /ve /t REG_SZ /d "${label}" /f`
@@ -20,6 +31,11 @@ function writeItem(name: string, label: string, command: string, icon: string) {
   execSync(
     `reg add "HKCU\\SOFTWARE\\Classes\\Directory\\background\\shell\\${name}\\command" /ve /t REG_SZ /d "${command}" /f`
   )
+  if (extended) {
+    execSync(
+      `reg add "HKCU\\SOFTWARE\\Classes\\Directory\\background\\shell\\${name}" /v Extended /t REG_SZ /d "" /f`
+    )
+  }
 }
 
 export default async function (key: string, label: string, exe: string) {
