8204212c78bea46146a9452313a514cdf8dbce6a
Nazmus Sayad <87106526+NazmusSayad@users.noreply.github.com>
2024-11-09 16:16:33 +0600

    final version

--- build.cjs
+++ build.cjs
@@ -1,14 +1,25 @@
 const fs = require('fs')
 const path = require('path')
+const exe = require('@angablue/exe')
 const { execSync } = require('child_process')
 
-console.log('Cleaning up...')
-fs.rmSync(path.resolve(__dirname, './dist'), { recursive: true, force: true })
+const OUT_DIR = path.resolve(__dirname, './dist')
+const OUT_FILE = path.resolve(OUT_DIR, './iwint.exe')
 
-console.log('')
-console.log('Building CommonJS module...')
+console.log('> Cleaning up...')
+fs.rmSync(OUT_DIR, { recursive: true, force: true })
+
+console.log('> Building CommonJS module...')
 execSync('tsc', { stdio: 'inherit' })
 
-console.log('')
-console.log('Building exe file...')
-execSync('npm run pkg', { stdio: 'inherit' })
+console.log('> Building exe file...')
+const build = exe({
+  out: OUT_FILE,
+  // icon: './icon.ico',
+  entry: path.join(OUT_DIR, './index.js'),
+})
+
+build.then(() => {
+  console.log('- Build complete!')
+  console.log('- Output:', OUT_FILE)
+})
